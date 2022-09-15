11f3a0fbe45a2ace14ef5f4dc9b654458d92028f
Usama Malik <113186340+usama-devloper@users.noreply.github.com>
2022-09-15 14:14:37 +0530

    Delete Assignments-1.dart

--- Assignments-1.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  
-  print("My name is Mohammad Usama Malik");
-  print("My birthday : 9th Feb 1995");
-  print("I am 26 Years old");
-  print("My Address : Kalupur , Ahmedabad ");
-  
-}
